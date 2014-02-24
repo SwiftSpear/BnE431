259be277ef265f3d91fb1518310e66d6ffaf2342
ben <ben@ben-VirtualBox.(none)>
2014-02-23 20:34:48 -0800

    last thing that compiled

--- assn2/PPPv3/parallel_for.h
+++ assn2/PPPv3/parallel_for.h
@@ -12,12 +12,18 @@
 
 namespace ppp {
   
-  template <typename T>
-  extern inline
-  void parallel_for(int64_t start, int64_t end, T* functor, int64_t grainsize=0)
-  {
-    // ASSIGNMENT: make this parallel via recursive divide and conquer
-    functor->calculate(start, end);
+  namespace internal {
+    template <typename T>
+    extern inline
+    void parallel_for(int64_t start, int64_t end, T* functor, int64_t grainsize=0)
+    {
+      // ASSIGNMENT: make this parallel via recursive divide and conquer
+      if (grainsize == 0) {
+        grainsize = (start-end+1) / (get_thread_count()*4);
+      }
+      PPP_DEBUG_MSG("parallel_for grainsize: " + to_string(grainsize));
+      functor->calculate(start, end);
+    }
   }
 }
 
