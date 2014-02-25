dd4940c7d0a97b63624c09023b9b808445f4a3d8
Elvis Isic <elvis.isic7@gmail.com>
2014-02-25 13:57:23 -0800

    working correctly now

--- assn2/PPPv3/TaskGroup.C
+++ assn2/PPPv3/TaskGroup.C
@@ -18,7 +18,7 @@ namespace ppp {
 
   void TaskGroup::spawn(Task& t) {
     assert(g_queues_ptr != NULL);
-    TaskQueue& queue = g_queues_ptr[0];  // ASSIGNMENT: use per-thread task queue with "get_thread_id()"
+    TaskQueue& queue = g_queues_ptr[get_thread_id()];  // ASSIGNMENT: use per-thread task queue with "get_thread_id()"
     m_wait_counter.fetch_and_inc();
     t.setCounter(&m_wait_counter);
     queue.enqueue(&t);
@@ -26,29 +26,33 @@ namespace ppp {
   
   void process_tasks(const atomic<int>* counter)
   {
-    TaskQueue& queue = g_queues_ptr[0];  // ASSIGNMENT: use per-thread task queue with "get_thread_id()"
+    TaskQueue& queue = g_queues_ptr[get_thread_id()];  // ASSIGNMENT: use per-thread task queue with "get_thread_id()"
 
     while (counter->get() != 0) {
       PPP_DEBUG_EXPR(queue.size());
        
       // Dequeue from local queue
       Task* task = queue.dequeue();
-      Task* x;
-      TaskGroup tg;
+      //Task* x;
+      //TaskGroup tg;
 
       // ASSIGNMENT: add task stealing
       if (task != NULL) {
+
         task->execute(); // overloaded method
         task->post_execute(); // cleanup, method of base class
+
       }
-      else{
-        PPP_DEBUG_MSG("Stealing ");
+
+      if (task == NULL){
+      	PPP_DEBUG_MSG("STEAL");
+      	task = queue.steal();
+      }
         
-        x = queue.steal();
-        //tg.spawn(x);
-        return;
+      	
       }
+      return;
     }
   }
-}
+
 
